f31df8e39e34b70f803e348db3320257d0e5ee84
Bryon Zandt <9594003+bdizzz@users.noreply.github.com>
2020-12-17 12:51:32 -0500

    add test scenario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-ie11';
+export class AppComponent implements OnInit {
+
+  public ngOnInit(): void {
+    document.documentElement.style.setProperty('--the-test', '#FF0000');
+  }
 }
